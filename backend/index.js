b5b065007fbabccf4ded9f315c14bc3e40e89a4b
Sreenivasan <sreeniezio18@gmail.com>
2024-12-20 22:11:14 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
+
