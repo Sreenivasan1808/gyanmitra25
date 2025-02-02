a45c20fc2c0da1eb58fc26f724cd0e9cda912af2
unknown <sarathyj121@gmail.com>
2025-02-02 23:43:54 +0530

    summa

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,23 +4,46 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const { Schema } = mongoose;
-const db=require("../index")
+const db = require("../index");
+
 const User = new Schema({
   user_id: { type: String, unique: true, required: true },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: [true, "Email-ID Already exist"], validate: [validator.isEmail, 'Please enter valid Email ID'] },
   gender: { type: String, required: true },
   password: { type: String, required: true, maxlength: [30, 'Password cannot exceed 30 characters'], select: false },
-  phone: { type: String, required: true  },
+  phone: { type: String, required: true },
   role: { type: String, default: "user" },
   cname: { type: String, required: true },
   ccity: { type: String, required: true },
-  eventPayed: { type: String, required: true,default:"Not Payed"},
-  workshopPayed: { type: String, required: true,default:"Not Payed" },
-
+  eventPayed: { type: String, required: true, default: "Not Payed" },
+  workshopPayed: { type: String, required: true, default: "Not Payed" },
   resetPasswordToken: { type: String },
   resetPasswordTokenExpire: { type: Date },
   createAt: { type: Date, default: Date.now },
+});
+
+// Function to generate the next user ID
+User.statics.generateUserId = async function () {
+  const lastUser = await this.findOne({}, {}, { sort: { 'createAt': -1 } });
+  let lastUserId = lastUser ? lastUser.user_id : 'GM0000';
+  const lastNumber = parseInt(lastUserId.replace('GM', ''), 10);
+  const nextNumber = lastNumber + 1;
+  return `GM${nextNumber.toString().padStart(4, '0')}`;
+};
+
+// Pre-save hook to set the user ID
+User.pre('save', async function (next) {
+  if (!this.isNew) {
+    return next();
+  }
+
+  try {
+    this.user_id = await this.constructor.generateUserId();
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 User.pre('save', async function (next) {
